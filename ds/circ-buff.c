90a72e420a3bdb8884d1612d9baa1387f0b3703a
Basavaraj M. Hubli <mh.basavaraj@globaledgesoft.com>
2018-02-16 18:11:35 +0530

    ds: Added circular buffer code

---
+++ ds/circ-buff.c
@@ -0,0 +1,120 @@
+
+#include "circ-buff.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief
+ *
+ * @param circ_buf
+ * @param max_elem
+ *
+ * @return
+ */
+int
+circ_buf_init(circ_buf_t **circ_buf, int max_elem)
+{
+    int buf_size;
+
+    *circ_buf = (circ_buf_t *) malloc(sizeof(circ_buf_t));
+    if (*circ_buf == NULL) {
+        return CIRC_BUF_ERR;
+    }
+
+    buf_size = sizeof(*(*circ_buf)->data) * max_elem;
+    (*circ_buf)->data = (int *) malloc(buf_size);
+    if ((*circ_buf)->data == NULL) {
+        return CIRC_BUF_ERR;
+    }
+
+    memset((*circ_buf)->data, 0, buf_size);
+    (*circ_buf)->head = (*circ_buf)->tail = 0;
+    (*circ_buf)->max_elem = max_elem;
+
+    return CIRC_BUF_NOERR;
+}
+
+/**
+ * @brief
+ *
+ * @param circ_buf
+ * @param elem
+ *
+ * @return
+ */
+int
+circ_buf_push(circ_buf_t *circ_buf, int elem)
+{
+    if (is_circ_buf_full(circ_buf)) {
+        return CIRC_BUF_FULL;
+    }
+
+    circ_buf->data[circ_buf->tail] = elem;
+    circ_buf->tail = (circ_buf->tail + 1) % circ_buf->max_elem;
+
+    return CIRC_BUF_NOERR;
+}
+
+/**
+ * @brief
+ *
+ * @param circ_buf
+ * @param elem
+ *
+ * @return
+ */
+int
+circ_buf_pop(circ_buf_t *circ_buf, int *elem)
+{
+    if (is_circ_buf_empty(circ_buf)) {
+        return CIRC_BUF_EMPTY;
+    }
+
+    *elem = circ_buf->data[circ_buf->head];
+    circ_buf->head = (circ_buf->head + 1) % circ_buf->max_elem;
+
+    return CIRC_BUF_NOERR;
+}
+
+/**
+ * @brief
+ *
+ * @param circ_buf
+ *
+ * @return
+ */
+inline bool
+is_circ_buf_empty(circ_buf_t *circ_buf)
+{
+    return (circ_buf->head == circ_buf->tail);
+}
+
+/**
+ * @brief
+ *
+ * @param circ_buf
+ *
+ * @return
+ */
+inline bool
+is_circ_buf_full(circ_buf_t *circ_buf)
+{
+    return (((circ_buf->tail + 1) % circ_buf->max_elem) == circ_buf->head);
+}
+
+/**
+ * @brief
+ *
+ * @param circ_buf
+ *
+ * @return
+ */
+int
+circ_buf_deinit(circ_buf_t *circ_buf)
+{
+    circ_buf->head = circ_buf->tail = circ_buf->max_elem = 0;
+    free(circ_buf->data);
+    free(circ_buf);
+
+    return CIRC_BUF_NOERR;
+}
